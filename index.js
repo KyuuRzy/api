09796b56926f99e91a75f37b1a307e6c9c6d677b
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-11 11:29:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -115,9 +115,7 @@ app.get('/api/Lirik', async (req, res) => {
     }
     const response = await quotes(message);
     res.status(200).json({
-      status: 200,
-      creator: "KyuuRzy",
-      data: { response } 
+     { response } 
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
