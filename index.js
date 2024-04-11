116a949bdf5881cfe53bcb3c889aca3fec589a52
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-11 12:19:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,51 +12,25 @@ app.set("json spaces", 2);
 // Middleware untuk CORS
 app.use(cors());
 
-async function joox(query) {
-	return new Promise((resolve, reject) => {
-		const time = Math.floor(new Date() / 1000)
-		axios.get('http://api.joox.com/web-fcgi-bin//web_search?lang=id&country=id&type=0&search_input=' + query + '&pn=1&sin=0&ein=29&_=' + time)
-			.then(({
-				data
-			}) => {
-				let result = []
-				let hasil = []
-				let promoses = []
-				let ids = []
-				data.itemlist.forEach(result => {
-					ids.push(result.songid)
-				});
-				for (let i = 0; i < data.itemlist.length; i++) {
-					const get = 'http://api.joox.com/web-fcgi-bin/web_get_songinfo?songid=' + ids[i]
-					promoses.push(
-						axios.get(get, {
-							headers: {
-								Cookie: 'wmid=142420656; user_type=1; country=id; session_key=2a5d97d05dc8fe238150184eaf3519ad;'
-							}
-						})
-						.then(({
-							data
-						}) => {
-							const res = JSON.parse(data.replace('MusicInfoCallback(', '').replace('\n)', ''))
-							hasil.push({
-								lagu: res.msong,
-								album: res.malbum,
-								penyanyi: res.msinger,
-								publish: res.public_time,
-								img: res.imgSrc,
-								mp3: res.mp3Url
-							})
-							Promise.all(promoses).then(() => resolve({
-								creator: "Arietube",
-								status: true,
-								data: hasil,
-							}))
-						}).catch(reject)
-					)
-				}
-			}).catch(reject)
-	})
+function tiktokdl(URL) {
+    return new Promise(async(resolve, rejecet) => {
+        let { data } = await axios.request({
+            url: "https://lovetik.com/api/ajax/search",
+            method: "POST",
+            data: new URLSearchParams(Object.entries({ query: URL }))
+        })
+        let result = {
+            desc: data.desc,
+            author: data.author,
+            author_name: data.author_name,
+            cover: data.cover,
+            video: data.play_url,
+            audio: data.links[4].a || "".replace("https", "http")
+        }
+        resolve(result)
+    })
 }
+
 
 // Fungsi untuk ragBot
 async function ragBot(message) {
@@ -133,13 +107,13 @@ async function blackboxAIChat(message) {
 }
 
 // Endpoint BingApi
-app.get('/api/joox', async (req, res) => {
+app.get('/api/tiktokdl', async (req, res) => {
   try {
     const message = req.query.message;
     if (!message) {
       return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
     }
-    const response = await joox(message);
+    const response = await tiktokdl(message);
     res.status(200).json({
      status: 200,
       creator: "KyuuRzy",
