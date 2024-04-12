ca6a44fde642ed122615a061f2f50839b4205b19
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-12 14:23:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -170,32 +170,6 @@ function quotes(input) {
     })
 }
 
-function witterdl2(link){
-	return new Promise((resolve, reject) => {
-		let config = {
-			'URL': link
-		}
-		axios.post('https://twdown.net/download.php',qs.stringify(config),{
-			headers: {
-				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-				"sec-ch-ua": '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
-				"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-				"cookie": "_ga=GA1.2.1388798541.1625064838; _gid=GA1.2.1351476739.1625064838; __gads=ID=7a60905ab10b2596-229566750eca0064:T=1625064837:RT=1625064837:S=ALNI_Mbg3GGC2b3oBVCUJt9UImup-j20Iw; _gat=1"
-			}
-		})
-		.then(({ data }) => {
-		const $ = cheerio.load(data)
-		resolve({
-				desc: $('div:nth-child(1) > div:nth-child(2) > p').text().trim(),
-				thumb: $('div:nth-child(1) > img').attr('src'),
-				HD: $('tbody > tr:nth-child(1) > td:nth-child(4) > a').attr('href'),
-				SD: $('tr:nth-child(2) > td:nth-child(4) > a').attr('href'),
-				audio: 'https://twdown.net/' + $('tr:nth-child(4) > td:nth-child(4) > a').attr('href')
-			})
-		})
-	.catch(reject)
-	})
-}
 async function facebook(url) {
     return new Promise((resolve, reject) => {
         axios({
@@ -411,24 +385,6 @@ async function blackboxAIChat(message) {
     throw error;
   }
 }
-
-// Endpoint TikTokDl
-app.get('/api/twtdl', async (req, res) => {
-  try {
-    const message = req.query.message;
-    if (!message) {
-      return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
-    }
-    const response = await witterdl2(message);
-    res.status(200).json({
-     status: 200,
-      creator: "KyuuRzy",
-      data: { response } 
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 // Endpoint TikTokDl
 app.get('/api/ghstalk', async (req, res) => {
