1dcdeb6e6cc02638df3fe2c9613a23ca4e37737b
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-12 15:17:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,39 +22,51 @@ app.set("json spaces", 2);
 app.use(cors());
 
 function igdl(url) {
-    return new Promise(async (resolve, reject) => {
-        axios.request({
-            url: 'https://www.instagramsave.com/download-instagram-videos.php',
-            method: 'GET',
-            headers: {
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                "cookie": "PHPSESSID=ugpgvu6fgc4592jh7ht9d18v49; _ga=GA1.2.1126798330.1625045680; _gid=GA1.2.1475525047.1625045680; __gads=ID=92b58ed9ed58d147-221917af11ca0021:T=1625045679:RT=1625045679:S=ALNI_MYnQToDW3kOUClBGEzULNjeyAqOtg"
-            }
-        })
-            .then(({ data }) => {
-                const $ = cheerio.load(data)
-                const token = $('#token').attr('value')
-                let config = {
-                    headers: {
-                        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                        "sec-ch-ua": '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
-                        "cookie": "PHPSESSID=ugpgvu6fgc4592jh7ht9d18v49; _ga=GA1.2.1126798330.1625045680; _gid=GA1.2.1475525047.1625045680; __gads=ID=92b58ed9ed58d147-221917af11ca0021:T=1625045679:RT=1625045679:S=ALNI_MYnQToDW3kOUClBGEzULNjeyAqOtg",
-                        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                    },
-                    data: {
-                        'url': url,
-                        'action': 'post',
-                        'token': token
-                    }
-                }
-                axios.post('https://www.instagramsave.com/system/action.php', qs.stringify(config.data), { headers: config.headers })
-                    .then(({ data }) => {
-                        resolve(data.medias)
+     return new Promise(async(resolve, reject) => {
+            let res = await axios("https://indown.io/");
+            let _$ = cheerio.load(res.data);
+            let referer = _$("input[name=referer]").val();
+            let locale = _$("input[name=locale]").val();
+            let _token = _$("input[name=_token]").val();
+            let { data } = await axios.post(
+              "https://indown.io/download",
+              new URLSearchParams({
+                link: url,
+                referer,
+                locale,
+                _token,
+              }),
+              {
+                headers: {
+                  cookie: res.headers["set-cookie"].join("; "),
+                },
+              }
+            );
+            let $ = cheerio.load(data);
+            let result = [];
+            let __$ = cheerio.load($("#result").html());
+            __$("video").each(function () {
+              let $$ = $(this);
+              result.push({
+                type: "video",
+                thumbnail: $$.attr("poster"),
+                url: $$.find("source").attr("src"),
+              });
+            });
+            __$("img").each(function () {
+              let $$ = $(this);
+              result.push({
+                type: "image",
+                url: $$.attr("src"),
+              });
+            });
+             return resolve({
+                    creator: global.creator,
+                    status: true,
+                    result
                     })
             })
-            .catch(reject)
-    })
-}
+          }
 
 async function igstalk(username) {
   try {
