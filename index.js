a9703496e1858907297eae8c6a30624eee1936b0
KyuuRzy Dev <160400183+KyuuRzy@users.noreply.github.com>
2024-04-17 02:51:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,39 @@ app.set("json spaces", 2);
 // Middleware untuk CORS
 app.use(cors());
 
+async function tiktokStalk2(user) {
+return new Promise(async(resolve, reject) => {
+const options = {
+method: 'POST',
+url: 'https://toolxox.com/seo/find-tiktok-account-analyze.php',
+headers: {
+"content-type": 'application/x-www-form-urlencoded',
+"user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+},
+formData: {
+url: user
+}
+}
+request(options, async function(error, response, body) {
+const $ = cheerio.load(body)
+if (!$('#profile > tbody > tr > td:nth-child(1)').text().replace(/\D/g, '')) return resolve({status: false, message: 'user not found'})
+const { data } = await axios.get(`https://urlebird.com/user/${user}/`)
+const $$ = cheerio.load(data)
+const result = {
+status: true,
+username: $$('body').find('div.col-md-auto.text-center.text-md-left.pl-0 > h1').text(),
+nickname: $$('body').find('div.col-md-auto.text-center.text-md-left.pl-0 > div > h5').text(),
+ppurl: $$('body').find('div.col-md-auto.justify-content-center.text-center > img').attr('src'),
+followers: $('#profile > tbody > tr > td:nth-child(1)').text().replace(/\D/g, ''),
+following: $('#profile > tbody > tr > td:nth-child(2)').text().replace(/\D/g, ''),
+likes: $('#profile > tbody > tr > td:nth-child(3)').text().replace(/\D/g, ''),
+videos: $('#profile > tbody > tr > td:nth-child(4)').text().replace(/\D/g, ''),
+}
+resolve(result)
+})
+})
+}
+
 function askGPT(message) {
 return new Promise(async (resolve, reject) => {
 	try {
@@ -516,14 +549,15 @@ app.get('/api/ghstalk', async (req, res) => {
   }
 });
 
+
 // Endpoint TikTokDl
-app.get('/api/quotes', async (req, res) => {
+app.get('/api/tiktokstalk2', async (req, res) => {
   try {
     const message = req.query.message;
     if (!message) {
       return res.status(400).json({ error: 'Example: Malam' });
     }
-    const response = await quotes(message);
+    const response = await tiktokStalk2(message);
     res.status(200).json({
      status: 200,
       creator: "KyuuRzy",
